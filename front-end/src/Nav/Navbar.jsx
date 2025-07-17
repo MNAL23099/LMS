a1ee83a6117cfed569f36d8890b119705e02ea5d
23099 <bsse23099@itu.edu.pk>
2025-07-17 17:19:30 +0500

    manal

--- front-end/src/Nav/Navbar.jsx
+++ front-end/src/Nav/Navbar.jsx
@@ -1,19 +1,20 @@
 import { useNavigate } from "react-router-dom";
 import "./Navbar.css";
+
 function Navbar(props){
 
     const navigate = useNavigate();
-    function goToHomePage(){
-        navigate("/homepage")
+
+    function goBack(){
+        navigate(-1);
     }
 
     return(
         <nav className="navbar bg-body-tertiary" id="navbar_id">
             <div className="container-fluid">
-                <b className="navbar-brand">{props.pageType}</b>
+                <b id="navbar-propType" className="navbar-brand">{props.pageType}</b>
                 {/* props.pageType means the type of page user is currently inside, like it can be homepage, dashboard, etc */}
-
-                
+                <button className="btn btn-outline-light" onClick={goBack}>Go Back</button>
             </div>
         </nav>
     );
