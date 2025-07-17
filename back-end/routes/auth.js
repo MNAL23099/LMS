ed472d4c35c2ab5464f7f937cd3b3b86be0aca2e
M. Umar Younas 43 <bsse23043@itu.edu.pk>
2025-07-17 16:56:33 +0500

    Fixes

--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { signUpUser, signInUser } = require("../controllers/authController");
+const signUpUser = require("../controllers/authController");
 
 
 // Route for signing up
