8b6baab2162f255ed845edfaafa32a4defc71607
M. Umar Younas 43 <bsse23043@itu.edu.pk>
2025-07-30 23:37:48 +0500

    Added image to FOE card inside admin dashboard

--- front-end/src/Admin/AdminDashboard.jsx
+++ front-end/src/Admin/AdminDashboard.jsx
@@ -1,7 +1,6 @@
 import Navbar from "../Nav/Navbar";
 import { useNavigate } from "react-router-dom";
-
-
+import image_FOE from "../assets/FOE.jpg";
 
 function AdminDashboard(){
 
@@ -16,12 +15,14 @@ function AdminDashboard(){
             <Navbar pageType="Admin Dashboard" />
 
             {/* Faculty Of Engineering */}
+            <div id="inventory-div_1">
             <div  onClick= {goToFOE}className="card" style={{width: "18rem"}}>
-            <img class="card-img-top" alt="..."/>
+            <img src={image_FOE} class="card-img-top" alt="..."/>
             <div className="card-body themed-card-body">
                 <h5 className="card-title">Faculty Of Engieering</h5>
                 <p className="card-text">Handle the inventory, lab staff of FOE labs.</p>
                 <a className="btn btn-primary">Go</a>
+            </div>
             </div>
             </div>
         
