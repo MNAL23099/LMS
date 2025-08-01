739ca6a5146ca5e68cce573f8e8cd74aa2be3dbd
M. Umar Younas 43 <bsse23043@itu.edu.pk>
2025-08-01 22:26:04 +0500

    Fixed add staff member, it now stores members inside university_staff table

--- back-end/controllers/staffController.js
+++ back-end/controllers/staffController.js
@@ -7,10 +7,10 @@ async function addStaff(req, res){
 
     //These are the name, email and designation of the staff member just added, we need email to make sure that
     //this staff member is not present in any other lab
-    const {name, email, designation} = req.body;
+    const {name, email, role} = req.body;
 
     //Return if either itemName or itemQuantity is null
-    if (await requestIsNotNull(name, email, designation) == false){
+    if (await requestIsNotNull(name, email, role) == false){
         console.log(`staffController.js -> addStaff(): requestIsNotNull() is returning false`);
         res.write("missing_entries");
         res.end();
@@ -21,31 +21,31 @@ async function addStaff(req, res){
     try {
         //If the staff member email is already inside, db, we won't add him, rather send a response that the staff member already exists
         if (await staffMemberAlreadyInDB(email) == true){
-            res.write("email_already_stored");
-            res.end();
-            return;
+          res.write("email_already_stored");
+          res.end();
+          return;
         }
         //But if the staff member email is not already present inside DB then create a new row for it
         else if(await staffMemberAlreadyInDB(email) == false) {
-            await addNewRow(name, await getLabName(), email, designation);
-            res.write("new_row_added");
-            res.end();
+          await addNewRow(name, email, role);
+          res.write("new_row_added");
+          res.end();
         }
         else { //In case none of the above conditions is running
-            console.log("error: addStaff()-> staffMemberAlreadyInDB returning null!");
-            res.write("error");
-            return;
+          console.log("error: addStaff()-> staffMemberAlreadyInDB returning null!");
+          res.write("error");
+          return;
         }
     }
     catch (error){
-        console.log(`error: staffController.js -> addStaff()-> ${error.message}`);
+      console.log(`error: staffController.js -> addStaff()-> ${error.message}`);
     }
 }
 
-async function requestIsNotNull(name, email, designation){ //This function returns true if the variables inside parameter 
+async function requestIsNotNull(name, email, role){ //This function returns true if the variables inside parameter 
   //are not null
 
-  if (name && email && designation){
+  if (name && email && role){
     return true;
   }
   else return false;
@@ -61,7 +61,7 @@ async function staffMemberAlreadyInDB(email) { //This function checks if itemNam
   const lsmClient = await connectToDB();
 
   try{
-    let query = "SELECT * FROM lab_staff WHERE email = $1";
+    let query = "SELECT * FROM university_staff WHERE email = $1";
     const data = await lsmClient.query(query, [email]);
 
     if (data.rowCount == 0){
@@ -76,14 +76,14 @@ async function staffMemberAlreadyInDB(email) { //This function checks if itemNam
   }
 }
 
-async function addNewRow(name, labName, email, designation){ //Add new row to the DB for this staff member name, his designation, email and its lab_name
+async function addNewRow(name, email, role){ //Add new row to the DB for this staff member name, his designation, email and its lab_name
 
   const lsmClient = await connectToDB();
 
   try{
-    const query = `INSERT INTO lab_staff(name, lab_name, email, designation)
-    VALUES($1, $2, $3, $4)`;
-    await lsmClient.query(query, [name, labName, email, designation]);
+    const query = `INSERT INTO university_staff(name, email, role)
+    VALUES($1, $2, $3)`;
+    await lsmClient.query(query, [name, email, role]);
   }
   catch (error){
     console.log(`error: staffController.js -> addNewRow()-> ${error.message}`);
